8f5c05bb95f4f2ff8fe212012501c7f0abd0852d
aaronjsharp <aaron.sharp2011@gmail.com>
2024-11-10 17:30:54 -0600

    Setup Appwrite

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,6 +1,10 @@
-import { Hono } from "hono" 
+import { Hono } from "hono"
+import { deleteCookie, setCookie } from "hono/cookie" 
 import { zValidator } from "@hono/zod-validator"
 import { loginSchema, registerSchema } from "../schemas"
+import { createAdminClient } from "@/lib/appwrite"
+import { ID } from "node-appwrite"
+import { AUTH_COOKIE } from "../constants"
 
 const app = new Hono()
   .post(
@@ -9,8 +13,20 @@ const app = new Hono()
     async (c) => {
       const { email, password } = c.req.valid("json")
 
-      console.log({ email, password })
-      return c.json({ email, password })
+      const { account } = await createAdminClient()
+      const session = await account.createEmailPasswordSession(
+        email,
+        password
+      )
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30
+      })
+      return c.json({ success: true })
     }
   )
   .post(
@@ -19,9 +35,33 @@ const app = new Hono()
     async (c) => {
       const { name, email, password } = c.req.valid("json")
 
-      console.log({ name, email, password })
-      return c.json({ name, email, password })
+      const { account } = await createAdminClient() 
+      await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      )
+
+      const session = await account.createEmailPasswordSession(
+        email,
+        password
+      )
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30
+      })
+      return c.json({ success: true })
     }
   )
+  .post("/logout", (c) => {
+    deleteCookie(c, AUTH_COOKIE)
+
+    return c.json({ success: true })
+  })
 
   export default app
