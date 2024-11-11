88cbac150e065596654692c90444ab377c35d240
aaronjsharp <aaron.sharp2011@gmail.com>
2024-11-11 17:28:48 -0600

    Sidebar and Navbar scaffolding

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -8,7 +7,7 @@ export default async function Home() {
   if (!user) redirect("/sign-in")
   return (
     <div>
-      <UserButton />
+      This is a Home page
     </div>
   );
 }
