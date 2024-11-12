79a354eb5eeb989b4a87ac067bac3c92b4599a69
aaronjsharp <aaron.sharp2011@gmail.com>
2024-11-11 20:06:42 -0600

    add createWorkspace functionality. Add toast notifications for login, logout, register, and createWorkspace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { QueryProvider } from '@/components/query-provider';
 
 import './globals.css';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={cn(inter.className, 'antialiased min-h-screen')}>
-        <QueryProvider>{children}</QueryProvider>
+        <QueryProvider>
+          <Toaster richColors />
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
